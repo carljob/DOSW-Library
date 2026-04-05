023edf7843c4013d61cc4d82ffb4fef025e38cf3
carljob <uribe.vargas.carlos@gmail.com>
2026-04-05 18:32:13 -0500

    se corrieron clases

--- src/main/java/edu/eci/dosw/DOSW_Library/DoswLibraryApplication.java
+++ src/main/java/edu/eci/dosw/DOSW_Library/DoswLibraryApplication.java
@@ -2,8 +2,16 @@ package edu.eci.dosw.DOSW_Library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(
+    basePackages = "edu.eci.dosw.DOSW_Library.persistence.relational.dao"
+)
+@EnableMongoRepositories(
+    basePackages = "edu.eci.dosw.DOSW_Library.persistence.nonrelational.repository"
+)
 public class DoswLibraryApplication {
 
     public static void main(String[] args) {
