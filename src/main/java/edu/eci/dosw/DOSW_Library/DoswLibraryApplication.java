cd22f2e4fdeb8f1e890aa6a1e4301f4fbe32f114
carljob <uribe.vargas.carlos@gmail.com>
2026-04-06 23:46:13 -0500

    se agrego cambios en la configuracion de la api

--- src/main/java/edu/eci/dosw/DOSW_Library/DoswLibraryApplication.java
+++ src/main/java/edu/eci/dosw/DOSW_Library/DoswLibraryApplication.java
@@ -2,19 +2,25 @@ package edu.eci.dosw.DOSW_Library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(
-    basePackages = "edu.eci.dosw.DOSW_Library.persistence.relational.dao"
-)
 @EnableMongoRepositories(
-    basePackages = "edu.eci.dosw.DOSW_Library.persistence.nonrelational.repository"
+        basePackages = "edu.eci.dosw.DOSW_Library.persistence.nonrelational.repository"
 )
 public class DoswLibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DoswLibraryApplication.class, args);
     }
+
+    @Configuration
+    @Profile("relational")
+    @EnableJpaRepositories(
+            basePackages = "edu.eci.dosw.DOSW_Library.persistence.relational.dao"
+    )
+    static class RelationalConfig {}
 }
---
+++ src/main/java/edu/eci/dosw/DOSW_Library/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package edu.eci.dosw.DOSW_Library.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "DOSW Library API",
+                version = "1.0",
+                description = "API de gestión de biblioteca con JWT"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
